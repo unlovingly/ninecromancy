2eb742b5f5b34cca17a2603d5b7caea308c48490
u8y <mail.address.is.unsteady@gmail.com>
2018-11-25 20:45:07 +0900

    updated models

---
+++ src/models/slip/Purchase.ts
@@ -0,0 +1,15 @@
+import { Item } from '@/models/slip/Item';
+
+export class Slip {
+  constructor(
+    public identity: string = "",
+    public number: string,
+    public description: string,
+    public senderId: string,
+    public receiverId: string,
+    public approvedAt: Date = new Date(),
+    public publishedAt: Date,
+    public items: Array<Item>,
+  ) {
+  }
+}
--- src/models/slip/Sales.ts
+++ src/models/slip/Sales.ts
@@ -3,8 +3,6 @@ import { Item } from '@/models/slip/Item';
 export class Slip {
   constructor(
     public identity: string = "",
-    public number: string,
-    // public description: string,
     public senderId: string,
     public receiverId: string,
     public approvedAt: Date = new Date(),
